c0a637624bfd440b63fd950a85aeeba0ca7ea5f7
LourdenB15 <lourdenbaydal15@gmail.com>
2023-03-17 15:11:48 +0800

    Add logic; sq quantity increase sq size decrease

--- app.js
+++ app.js
@@ -4,44 +4,42 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const para = document.querySelector('p');
 
-
-
-
-
 window.addEventListener('load', (e) => {
-
     const initDimension = range.value;
     for (let i = 1; i <= initDimension; i++) {
+        const boxMeasure = `${right.offsetWidth / initDimension}px`;
         const r = document.createElement('div');
         for (let i = 1; i <= initDimension; i++) {
             const c = document.createElement('div');
             c.classList.add('inSq');
-
+            c.style.width = boxMeasure;
+            c.style.height = boxMeasure;
             r.appendChild(c);
         }
         right.appendChild(r)
     }
-
 });
+
 range.addEventListener('change', () => {
+    const initDimension = range.value;
     while (right.lastChild) {
         right.lastChild.remove();
     }
     dimension.forEach((e) => {
         e.textContent = range.value;
     });
-    const initDimension = range.value;
     for (let i = 1; i <= initDimension; i++) {
+        const boxMeasure = `${right.offsetWidth / initDimension}px`
         const r = document.createElement('div');
         for (let i = 1; i <= initDimension; i++) {
             const c = document.createElement('div');
             c.classList.add('inSq');
-
+            c.style.width = boxMeasure;
+            c.style.height = boxMeasure;
             r.appendChild(c);
         }
         right.appendChild(r)
     }
 });
-
 
 
