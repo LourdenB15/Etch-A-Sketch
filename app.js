c4e27d18b6d29fdcd01e2102ef0a8f6d0cabfb74
LourdenB15 <lourdenbaydal15@gmail.com>
2023-03-19 11:02:09 +0800

    Put mouseover event into function

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const right = document.querySelector('.right');
 const para = document.querySelector('p');
 
 window.addEventListener('load', (e) => {
+    boxGenerator();
+});
+
+range.addEventListener('change', () => {
+    while (right.lastChild) {
+        right.lastChild.remove();
+    }
+    dimension.forEach((e) => {
+        e.textContent = range.value;
+    });
+    boxGenerator();
+});
+
+function boxGenerator() {
     const initDimension = range.value;
     for (let i = 1; i <= initDimension; i++) {
         const boxMeasure = `${right.offsetWidth / initDimension}px`;
@@ -15,43 +29,14 @@ window.addEventListener('load', (e) => {
             c.style.width = boxMeasure;
             c.style.height = boxMeasure;
             r.appendChild(c);
-
         }
         right.appendChild(r)
         const sqs = document.getElementsByClassName('inSq');
         for (let i = 0; i < sqs.length; i++) {
+
             sqs[i].addEventListener('mouseover', () => {
                 sqs[i].style.backgroundColor = 'black';
-            })
+            });
         }
     }
-});
-
-range.addEventListener('change', () => {
-    const initDimension = range.value;
-    while (right.lastChild) {
-        right.lastChild.remove();
-    }
-    dimension.forEach((e) => {
-        e.textContent = range.value;
-    });
-    for (let i = 1; i <= initDimension; i++) {
-        const boxMeasure = `${right.offsetWidth / initDimension}px`
-        const r = document.createElement('div');
-        for (let i = 1; i <= initDimension; i++) {
-            const c = document.createElement('div');
-            c.classList.add('inSq');
-            c.style.width = boxMeasure;
-            c.style.height = boxMeasure;
-            r.appendChild(c);
-
-        }
-        right.appendChild(r)
-        const sqs = document.getElementsByClassName('inSq');
-        for (let i = 0; i < sqs.length; i++) {
-            sqs[i].addEventListener('mouseover', () => {
-                sqs[i].style.backgroundColor = 'black';
-            })
-        }
-    }
-});
+}
